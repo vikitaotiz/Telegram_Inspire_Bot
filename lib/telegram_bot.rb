897ce9bce06e3be79fbda74157fe5ab67b7a5d1c
Victor Otieno <victorotieno598@gmail.com>
2021-05-21 16:39:48 +0300

    Fix linters

--- lib/telegram_bot.rb
+++ lib/telegram_bot.rb
@@ -14,26 +14,25 @@ class Telebot
         case message.text
         when '/start'
           bot.api.send_message(chat_id: message.chat.id,
-            text: "Hello, #{message.from.first_name} , welcome to Vikitaotiz bot created by Victor,
-            the chat bot is to keep you inspired and entertained. Use  /start to start the bot,
-            /stop to end the bot, /inspire to get a diffrent motivational quote everytime you request
-            for it or /joke to get a joke everytime you request for it")
+                               text: "Hello, #{message.from.first_name} , welcome to Vikitaotiz bot created by Victor,
+                                         the chat bot is to keep you inspired and entertained. Use  /start to start the bot,
+                                         /stop to end the bot, /inspire to get a diffrent motivational quote everytime you request
+                                        for it or /joke to get a joke everytime you request for it")
         when '/stop'
           bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}", date: message.date)
         when '/inspire'
           values = Inspire.new
           value = values.any_inspiration
           bot.api.send_message(chat_id: message.chat.id, text: (value['text']).to_s, date: message.date)
-          bot.api.send_message(chat_id: message.chat.id, text: (value['author']).to_s, date: message.date)
+          bot.api.send_message(chat_id: message.chat.id, text: "Author: #{value['author']}", date: message.date)
 
         when '/joke'
           values = Pun.new
           value = values.request_pun
-          bot.api.send_message(chat_id: message.chat.id, text: (value['setup']).to_s, date: message.date)
-          bot.api.send_message(chat_id: message.chat.id, text: (value['punchline']).to_s, date: message.date)
+          bot.api.send_message(chat_id: message.chat.id, text: "#{value['setup']} - #{value['punchline']}", date: message.date)
         else
           bot.api.send_message(chat_id: message.chat.id,
-              text: "Invalid entry, #{message.from.first_name}, you need to use  /start,  /stop , /motivate or /joke")
+                               text: "Invalid entry, #{message.from.first_name}, you need to use  /start,  /stop , /motivate or /joke")
         end
       end
     end
