2690b90f4143e9bb1cb748cf2c7a96492b248587
Victor Otieno <victorotieno598@gmail.com>
2021-05-21 14:57:42 +0300

    Add pun_spec.rb file for testing jokes

---
+++ spec/pun_spec.rb
@@ -0,0 +1,16 @@
+require_relative '../lib/joke.rb'
+
+describe Pun do
+  describe '#make_the_request' do
+    let(:request) { Pun.new }
+    let(:request_json) { request.request_pun }
+    
+    it 'return hash response when the request is sucessful' do
+      expect(request_json.class).to eql(Hash)
+    end
+
+    it 'returns an array response should not be empty' do
+      expect(request_json.length).not_to eql(0)
+    end
+  end
+end
