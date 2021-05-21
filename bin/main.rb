081ba1c44486f84d63b4055fb5b7a0a0e5e2ff84
Victor Otieno <victorotieno598@gmail.com>
2021-05-21 15:09:33 +0300

    Add logic to main.rb file

---
+++ bin/main.rb
@@ -0,0 +1,15 @@
+require_relative '../lib/telegram_bot.rb'
+require_relative '../lib/inspire.rb'
+
+puts 'Hello! , Welcome to telegram bot that will inspire you'
+puts '----------------------------------------------'
+puts 'This is a bot to help you stay inspired by giving you inspirational quotes'
+puts 'The bot can also keep you entertained by providing you with jokes'
+puts 'This bolt will help you when you feeling unmotivated'
+
+sleep(0.6)
+
+puts '----------------------------------------------'
+puts '...bot is now online... use ctrl-c to stop the bot'
+
+Telebot.new
